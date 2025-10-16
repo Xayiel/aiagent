da63f3a53a8f519b1ded14d145ebf974214a6f52
Xayiel <izmyran@gmail.com>
2025-10-16 11:51:06 +0200

    started work: agent

--- functions/call_function.py
+++ functions/call_function.py
@@ -6,11 +6,9 @@ from functions.write_file import write_file
 
 def call_function(function_call_part, verbose=False): # function call part is a types.FunctionCall has .name and .args property
     print(f"Calling function: {function_call_part.name}({function_call_part.args})") if verbose is True else print(f" - Calling function: {function_call_part.name}")
-    # Call the function based on the given name, manually add WD(./calculator) to the dict of kwargs, syntax to pass a dict into a function using kwargs is: some_function(**some_args)
-    # {function name, "(string)"} -> function
     function_name = function_call_part.name
     function_args = function_call_part.args.copy()
-    function_args.update({"working_directory":"./calculator"})
+    function_args["working_directory"] = "./calculator"
     function_map = {"get_file_content":get_file_content, "get_files_info":get_files_info, "run_python_file":run_python_file, "write_file":write_file}
     func = function_map.get(function_name)
 
@@ -27,7 +25,7 @@ def call_function(function_call_part, verbose=False): # function call part is a 
     else:
         function_result = func(**function_args)
         return types.Content(
-            role="tool",
+            role="tool",                                                                                                    
             parts=[
                 types.Part.from_function_response(
                     name=function_name,
--- main.py
+++ main.py
@@ -27,24 +27,60 @@ When a user asks a question or makes a request, make a function call plan. You c
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    types.Content( # message object, see it as a container for our message conversation, knows who send the message and what the message is
+        role="user", # tells our ai its user input      
+        parts=[types.Part(text=user_prompt)] # creates a list of "parts", in this case just a text part for the AI to process
+        ),
 ]
 
 def main():
-    available_functions = types.Tool(function_declarations=[schema_get_files_info, schema_run_python_file, schema_write_file, schema_get_file_content])
-    config = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=config)
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_run_python_file, 
+            schema_write_file, 
+            schema_get_file_content
+            ]
+        )
+    config = types.GenerateContentConfig(
+        tools=[available_functions], 
+        system_instruction=system_prompt
+        )
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages, 
+        config=config
+        )
     usage = response.usage_metadata
-    
-    if "--verbose" in sys.argv:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {usage.prompt_token_count}")
-        print(f"Response tokens: {usage.candidates_token_count}")
-    if response.function_calls != None:
+    # Check candidates property of response, a list of response variations(usually1). Should contain "i want to call get_files_info", add it to our 
+    # conversation. Iterate over each candidate and add its .content to your messages list.
+    map(lambda candidate: messages.append(candidate.content), response.candidates)
+
+    verbose = "--verbose" in sys.argv
+    try:
+        if not response.function_calls:
+            raise Exception("Model didn't return any function calls")
         for function_call in response.function_calls:
-            # print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(response.text)
+            result = call_function(function_call, verbose=verbose)
+            # After each function call, use types.Content to convert function_responses into a message with a role of "user" and append it to messages.
+            
+            # check if result exists
+            if result is None:
+                raise Exception("No result from function call")
+            # check if parts is present and not empty
+            if result.parts is None or len(result.parts) <= 0:
+                raise Exception("Missing parts in the function call result")
+            # check function_response exists
+            parts_ob = result.parts[0] # parts[0] is a types.Part object which contains an attribute of function_response object.
+            if parts_ob.function_response is None:
+                raise Exception("Missing function response in result")
+            # check if response exists
+            if parts_ob.function_response.response is None:
+                raise Exception("Missing function_response.response in result")
+            if verbose:
+                print(f"-> {parts_ob.function_response.response}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
